9da3a3a6327a2f278c5511bd7b272bc08ac630e6
CHANGMOK LEE <cmlee5075@gmail.com>
2025-03-17 23:32:39 +0900

    initial commit

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -1,3 +1,25 @@
+<<<<<<< HEAD
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+=======
+<<<<<<< HEAD
+import globals from 'globals';
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+>>>>>>> bf0985a (initial commit)
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
+=======
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
@@ -10,3 +32,4 @@ export default [
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
+>>>>>>> 0ee79a9 (initial commit)
