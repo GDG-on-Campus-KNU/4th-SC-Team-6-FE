418f4bda0d17c6142cb9772f5fd0281f98c2197a
Catsmanager <hyunhyun0096@naver.com>
2025-03-18 00:50:34 +0900

    feat: Add PlayButton component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,12 @@ import Navigation from './components/Navigation';
 import AppRouter from './routes/Routers';
 import Header from './components/Header';
 
-//test
-
-import PlayButton from './components/PlayButton';
-import WearableButton from './components/WearableButton';
-
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <AppRouter />
       <Navigation />
-      <PlayButton to="/"></PlayButton>
-
-      <WearableButton />
     </BrowserRouter>
   );
 }
--- my-app/src/components/PlayButton.jsx
+++ my-app/src/components/PlayButton.jsx
@@ -1,15 +1,20 @@
 import { useNavigate } from "react-router-dom";
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 const PlayButton = ({ to, children = "합주하기", className = "" }) => {
 =======
 const PlayButton =({to,children})=>{
 >>>>>>> a7103de (fix: 충돌해결)
+=======
+const PlayButton = ({ to, children = "합주하기", className = "" }) => {
+>>>>>>> 49217f1 (feat: Add PlayButton component)
   const navigate = useNavigate();
 
   const handleClick = () => {
     navigate(to);
   };
+<<<<<<< HEAD
 <<<<<<< HEAD
 
   return (
@@ -54,3 +59,35 @@ return(
   </button>
 )
 >>>>>>> a7103de (fix: 충돌해결)
+=======
+
+  return (
+    <div className={`${className}`}>
+      <div
+        className="
+          relative w-[143px] h-[60px] sm:w-[120px] sm:h-[50px] xs:w-[100px] xs:h-[40px] 
+          cursor-pointer"
+        onClick={handleClick}
+      >
+        
+        <div className="
+          absolute inset-0 rounded-[30px] bg-[#203f9a] shadow-md 
+          transition-transform duration-200 active:scale-95
+        " />
+
+      
+        <p className="
+          absolute inset-0 flex items-center justify-center 
+          text-[25px] sm:text-[20px] xs:text-[16px] font-bold text-[#f7f2eb]
+        ">
+          {children}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PlayButton;
+
+
+>>>>>>> 49217f1 (feat: Add PlayButton component)
