c27c233164800f87771c323e8168863963ede85c
Catsmanager <hyunhyun0096@naver.com>
2025-03-23 10:15:52 +0900

    refactor: 일반 함수로 변경 및 버튼 스타일 변경

--- my-app/src/components/RecordButton.jsx
+++ my-app/src/components/RecordButton.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { FaMicrophone } from 'react-icons/fa';
+import { GoDotFill } from 'react-icons/go';
 
 const RecordButton = () => {
   const [isRecording, setIsRecording] = useState(false);
@@ -11,9 +11,12 @@ const RecordButton = () => {
   return (
     <button
       onClick={handleRecordClick}
-      className={`flex h-[60px] w-[60px] items-center justify-center rounded-full text-white shadow-lg ${isRecording ? 'bg-red-500' : 'bg-blue-500'} transition hover:bg-opacity-80`}
+      className={`flex h-[60px] w-[60px] items-center justify-center rounded-full shadow-lg ${isRecording ? 'bg-red-500' : 'bg-black bg-opacity-50'} `}
     >
-      <FaMicrophone size={24} />
+      <GoDotFill
+        size={60}
+        color="red"
+      />
     </button>
   );
 };
