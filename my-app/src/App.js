4e092e7a21a95ad1c0976e7c25fe0b0926de35e5
Catsmanager <hyunhyun0096@naver.com>
2025-03-31 21:43:33 +0900

    chore: 불필요한 파일 삭제

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,23 +4,12 @@ import Navigation from './components/Navigation';
 import AppRouter from './routes/Routers';
 import Header from './components/Header';
 
-//test
-
-import PlayButton from './components/PlayButton';
-import MusicCard from './components/MusicCard';
-import MusicCardBig from './components/MusicCardBig';
-
-
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <AppRouter />
       <Navigation />
-      <PlayButton to="/"></PlayButton>
-      <MusicCard titile="Oasis - Live Fore.." to ="/"/>
-      <MusicCardBig title="먼지가 되어 Cover" to="/" />
-
     </BrowserRouter>
   );
 }
