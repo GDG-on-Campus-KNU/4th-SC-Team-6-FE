634484486c4a5d4cc95ef2b8d2dd9776ef9be06a
CHANGMOK LEE <cmlee5075@gmail.com>
2025-04-03 20:40:18 +0900

    feat: 타이틀, 파티클 추가(WIP)

--- my-app/src/pages/Home/Animation3D.jsx
+++ my-app/src/pages/Home/Animation3D.jsx
@@ -2,27 +2,12 @@ import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { gsap } from 'gsap';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+
+// import getFont from '../../assets/fonts/fontText.json';
+import particleTexture from '../../assets/textures/particle.png';
 export default function Animation3D() {
-  function initCube() {
-    const cubeGeometry = new THREE.IcosahedronGeometry(1); // 높이, 깊이, 너비
-    const cubeMaterial = new THREE.MeshLambertMaterial({
-      color: 0xffffff, // color: '0xcc99ff' or 'coral' or new THREE.Color(0xcc99ff)
-      emissive: '#81D8CF', // emissive color
-    });
-    return new THREE.Mesh(cubeGeometry, cubeMaterial);
-  }
-
-  function initSkeleton() {
-    const skeletonGeometry = new THREE.IcosahedronGeometry(2);
-    const skeletonMaterial = new THREE.MeshBasicMaterial({
-      wireframe: true,
-      transparent: true,
-      opacity: 0.5,
-      color: 0xaaaaaa,
-    });
-    return new THREE.Mesh(skeletonGeometry, skeletonMaterial);
-  }
-
   function getSize() {
     const width = mountRef.current.clientWidth;
     const height = mountRef.current.clientHeight;
@@ -36,17 +21,11 @@ export default function Animation3D() {
     if (!container) return;
 
     const size = getSize();
-    const backgroundColor = { r: 0, g: 0, b: 0 };
 
     // Scene, Camera, Renderer 생성
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(size.width, size.height);
     container.appendChild(renderer.domElement); // DOM에 추가
-
-    // 배경 클리어(초기화)
-    renderer.setClearColor(
-      new THREE.Color(backgroundColor.r, backgroundColor.g, backgroundColor.b)
-    );
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
@@ -55,97 +34,152 @@ export default function Animation3D() {
       0.1,
       1000
     );
-    camera.position.z = 5; // 카메라 위치 설정
+    camera.position.z = (0, 1, 5); // 카메라 위치 설정
 
     // OrbitControls
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.autoRotate = true;
-    controls.enableDamping = true;
+    // const controls = new OrbitControls(camera, renderer.domElement);
+    // controls.autoRotate = true;
+    // controls.enableDamping = true;
 
-    // cube, skeleton 추가
-    const cube = initCube();
-    const skeleton = initSkeleton();
-    scene.add(cube, skeleton);
+    /** font */
+    const fontLoader = new FontLoader();
+
+    const loadFont = async () => {
+      try {
+        // 브라우저 기준으로 접근할 수 있는 절대 경로(/)여야
+        const font = await fontLoader.loadAsync('/assets/fonts/fontText.json');
+
+        const textGeometry = new TextGeometry('Feel Your Melody', {
+          // fontLoader와 비슷하게 개별경로에서 import
+          font,
+          size: 0.3,
+          height: 0.1,
+          bevelEnabled: true,
+          bevelSegments: 5,
+          bevelSize: 0.02,
+          bevelThickness: 0.02,
+        });
+        const textMaterial = new THREE.MeshPhongMaterial();
+        const text = new THREE.Mesh(textGeometry, textMaterial);
+        scene.add(text);
+        text.scale.set(1, 1, 0.005);
+
+        text.castShadow = true;
+        console.log('font loaded');
+
+        textGeometry.computeBoundingBox(); // text size and position update
+
+        // 가운데 정렬(단순히 가운데 정렬만을 목적으로 하면 textGeometry.center() 사용)
+        textGeometry.translate(
+          -(textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x) *
+            0.5,
+          -(textGeometry.boundingBox.max.y - textGeometry.boundingBox.min.y) *
+            -4,
+          -(textGeometry.boundingBox.max.z - textGeometry.boundingBox.min.z) *
+            0.5
+        );
+      } catch (error) {
+        console.error('폰트 로딩 실패:', error);
+      }
+    };
+    loadFont();
+
+    // particles 추가
+    const particlesGeometry = new THREE.BufferGeometry();
+
+    const count = 10000;
+    const positions = new Float32Array(count * 3);
+    const colors = new Float32Array(count * 3);
+
+    for (let i = 0; i < count; i++) {
+      // -0.5 - 0.5 사이 값 반환 randFloatSpread
+      positions[i * 3] = THREE.MathUtils.randFloatSpread(1);
+      positions[i * 3 + 1] = THREE.MathUtils.randFloatSpread(1);
+      positions[i * 3 + 2] = THREE.MathUtils.randFloatSpread(1);
+
+      colors[i * 3] = Math.random();
+      colors[i * 3 + 1] = Math.random();
+      colors[i * 3 + 2] = Math.random();
+    }
+
+    particlesGeometry.setAttribute(
+      'position',
+      new THREE.BufferAttribute(positions, 3)
+    );
+    particlesGeometry.setAttribute(
+      'color',
+      new THREE.BufferAttribute(colors, 3)
+    );
+    const particlesMaterial = new THREE.PointsMaterial({
+      color: 0xccaaff,
+      size: 2, // point size
+      sizeAttenuation: false, // 원근법 무시
+      vertexColors: true, // 정점의 색을 사용하겠다는 설정
+    });
+
+    const TextureLoader = new THREE.TextureLoader();
+    const texture = TextureLoader.load(particleTexture);
+    particlesMaterial.alphaMap = texture; // map -> alphaMap으로 변경
+    particlesMaterial.transparent = true;
+    particlesMaterial.depthWrite = false; // depth buffer에서 depth 정보 비활성화
+
+    const points = new THREE.Points(particlesGeometry, particlesMaterial);
+    scene.add(points);
 
     // light 추가
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8); // color, intensity(강도)
-    scene.add(directionalLight);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
 
-    // ------------------ for pelse
-
-    // 메트로놈 효과과
-    setInterval(() => {
-      pulseBackground();
-    }, 1000); // 1초마다 (BPM 60 기준)
-
-    // 배경색 반짝이게 하기
-    function pulseBackground() {
-      gsap.to(backgroundColor, {
-        r: 0.1,
-        g: 0.6,
-        b: 0.7,
-        duration: 0.2,
-        onUpdate: updateBackgroundColor,
-        onComplete: () => {
-          gsap.to(backgroundColor, {
-            r: 0,
-            g: 0,
-            b: 0,
-            duration: 0.4,
-            onUpdate: updateBackgroundColor,
-          });
-        },
-      });
-    }
-
-    // 배경색 업데이트
-    function updateBackgroundColor() {
-      const color = new THREE.Color(
-        backgroundColor.r,
-        backgroundColor.g,
-        backgroundColor.b
-      );
-      renderer.setClearColor(color);
-    }
-
+    // ------------------ for pulse
     // 예시: 사용자가 타이밍에 맞춰 입력했다고 가정하고 피드백 제공
     // 나중에 음의 정확도 인식하는 기능으로 개발 예정정
-    document.addEventListener('keydown', (e) => {
-      if (e.code === 'Space') {
-        giveFeedback(true); // 정확한 타이밍 입력 시
-      }
-    });
+    // setInterval(() => {
+    //   giveFeedback(true);
+    // }, 1000); // 1초마다 (BPM 60 기준)
 
     // 사용자의 타이밍 피드백
     function giveFeedback(isCorrect) {
       if (isCorrect) {
-        gsap.to(cube.scale, {
-          x: 1.5,
-          y: 1.5,
-          z: 1.5,
+        gsap.to(points.scale, {
+          x: 2,
+          y: 2,
+          z: 2,
           duration: 0.2,
           yoyo: true,
           repeat: 1,
         });
       }
     }
-    // for pelse endpoint
 
     const clock = new THREE.Clock();
+    let previousFeedbackTime = 0;
+
+    // 초기 BPM 설정 (예: 60)
+    let bpm = 50;
+    let feedbackInterval = 60 / bpm;
+
+    // BPM 변경 함수
+    function setBPM(newBPM) {
+      bpm = newBPM;
+      feedbackInterval = 60 / bpm;
+    }
 
     // 애니메이션 루프
     function animate() {
       const elapsedTime = clock.getElapsedTime();
 
-      // elapsed time since the clock was started in seconds
-      cube.rotation.x = elapsedTime;
-      cube.rotation.y = elapsedTime;
+      // BPM에 따라 피드백 간격 다르게 실행
+      if (elapsedTime - previousFeedbackTime >= feedbackInterval) {
+        giveFeedback(true);
+        previousFeedbackTime = elapsedTime;
+      }
 
-      skeleton.rotation.x = elapsedTime * 1.5;
-      skeleton.rotation.y = elapsedTime * 1.5;
+      // elapsed time since the clock was started in seconds
+      points.rotation.x = elapsedTime;
+      points.rotation.y = elapsedTime;
 
       // OrbitControls: autoRotate 사용시시 호출해줘야 결과 반영
-      controls.update();
+      // controls.update();
       renderer.render(scene, camera);
 
       requestAnimationFrame(animate);
@@ -165,12 +199,13 @@ export default function Animation3D() {
 
     // Cleanup (컴포넌트 언마운트 시)
     return () => {
-      window.removeEventListener('keydown', giveFeedback);
       window.removeEventListener('resize', handleResize);
       container.removeChild(renderer.domElement);
       renderer.dispose();
+      // controls.dispose();
     };
   }, []);
+
   return (
     <div className="flex h-screen w-full items-center justify-center">
       <div
