c643fe30c0b296f4d515ebf8b1ee525627ba9f36
Changmok Lee <103271836+eelkom@users.noreply.github.com>
2025-04-03 19:37:10 +0900

    Style/global background update (#4)
    * initial commit
    
    * chore: update Prettier and ESLint configurations
    
    * style: App.js 배경 컬러 업데이트
    
    ---------
    
    Co-authored-by: Hyunji <hyunhyun0096@naver.com>

--- my-app/src/pages/Community.js
+++ my-app/src/pages/Community.js
@@ -1,7 +1,7 @@
-export default function ommunity() {
+export default function Community() {
   return (
-    <>
-      <h1>Community Page</h1>
-    </>
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold text-gray-800">Community Page</h1>
+    </div>
   );
 }
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold text-gray-800">Home Page</h1>
+    </div>
   );
 }
