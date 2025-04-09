d52dd2d93903501a3622f62094ae63d909097ed2
CHANGMOK LEE <cmlee5075@gmail.com>
2025-04-09 18:39:22 +0900

    chore: my-app 디렉토리 제거 및 프로젝트 루트로 파일 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Navigation from './components/Navigation';
-
 import Header from './components/Header';
 import AppRouter from './routes/Routers';
 
@@ -11,9 +10,9 @@ function App() {
       {/* gradient blur 1 */}
       <div className="absolute top-[160.11px] left-[30px] h-[319.89px] w-[321.83px] bg-gradient-to-b from-[#81D8CF] to-[rgba(4,125,230,0.46)] blur-[100px]"></div>
       <div className="absolute top-[141px] left-[128.51px] h-[234.06px] w-[235.49px] bg-gradient-to-b from-[#81D8CF] to-[#7061A3] blur-[50px]"></div>
-
       {/* gradient blur 2 */}
       <div className="absolute top-[50vh] left-[65vw] h-[234.06px] w-[235.49px] bg-gradient-to-b from-[#81D8CF] to-[#7061A3] blur-[75px]"></div>
+
       <BrowserRouter>
         <Header />
         <Navigation />
