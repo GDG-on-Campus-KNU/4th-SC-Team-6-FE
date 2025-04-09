53c4ead36a1abd6e4a709fc61d8df90038eb48d7
CHANGMOK LEE <cmlee5075@gmail.com>
2025-04-09 19:19:25 +0900

    style: header max width 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <div className="relative top-0 left-0 z-[11] h-[44px] w-full px-4 backdrop-blur-[10px]">
-      <nav className="mx-auto flex h-full max-w-[1000px] items-center">
+      <nav className="mx-auto flex h-full max-w-[850px] items-center">
         <Link to="/" className="mr-auto text-[1.2rem] font-extrabold">
           The Feelody
         </Link>
