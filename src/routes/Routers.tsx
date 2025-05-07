9c2ea06a25b34307fa8fe88a7eb1809f388c9efd
Catsmanager <hyunhyun0096@naver.com>
2025-05-07 23:54:13 +0900

    feat: 라우팅 추가

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/home';
-import Community from '../pages/community';
 import Feedback from '../pages/feedback';
+import FeedbackDetailPage from '../pages/feedback/DetailPage';
 import MyPage from '../pages/myPage';
 import AboutUs from '../pages/aboutUs';
 
@@ -9,8 +9,8 @@ export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/community" element={<Community />} />
       <Route path="/feedback" element={<Feedback />} />
+      <Route path="/feedback/:id" element={<FeedbackDetailPage />} />
       <Route path="/myPage" element={<MyPage />} />
       <Route path="/aboutUs" element={<AboutUs />} />
     </Routes>
