d606f52da70a4e753f750619a49a950a45dc8618
Catsmanager <hyunhyun0096@naver.com>
2025-05-08 23:23:30 +0900

    feat: 라우팅추가

--- src/routes/Routers.tsx
+++ src/routes/Routers.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/home';
 import Feedback from '../pages/feedback';
 import FeedbackDetailPage from '../pages/feedback/DetailPage';
+import Community from '../pages/community';
 import MyPage from '../pages/myPage';
 import AboutUs from '../pages/aboutUs';
 
@@ -11,6 +12,7 @@ export default function AppRouter() {
       <Route path="/" element={<Home />} />
       <Route path="/feedback" element={<Feedback />} />
       <Route path="/feedback/:id" element={<FeedbackDetailPage />} />
+      <Route path="/community" element={<Community />} />
       <Route path="/myPage" element={<MyPage />} />
       <Route path="/aboutUs" element={<AboutUs />} />
     </Routes>
