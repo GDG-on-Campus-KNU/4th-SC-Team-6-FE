8437177cc525041b5253ff6bc008cfe4c0416f9c
CHANGMOK LEE <cmlee5075@gmail.com>
2025-05-13 03:19:06 +0900

    feat: 피드백 에니메이션 수정 및 추가

--- src/pages/home/components/Animation3D.tsx
+++ src/pages/home/components/Animation3D.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import { loadCharacter } from '../utils/loadCharacter';
-import { addNoteText, loadFont } from '../utils/loadFont';
+import { loadFont } from '../utils/loadFont';
 import { loadParticles } from '../utils/loadParticles';
 import { loadLight } from '../utils/loadLight';
 import { getSize } from '../utils/getSize';
 import { getCanvasText } from '../utils/getCanvasText';
+import { createNoteBurts, createNoteWave } from '../utils/loadAnimations';
 import RecordButton from './RecordButton';
 import WearableButton from './WearableButton';
 import Metronome from './Metronome';
@@ -17,13 +18,23 @@ export default function Animation3D() {
   const sceneRef = useRef<THREE.Scene | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  const particleGroupsRef = useRef<THREE.Points[]>([]);
+  const particleRef = useRef<THREE.Points | null>(null);
   const [ready, setReady] = useState(false);
   const [isRecording, setIsRecording] = useState(false);
   const [bpm, setBpm] = useState<number>(50);
+  const [isSceneReady, setIsSceneReady] = useState(false);
   const loadingManager = new THREE.LoadingManager();
 
   const [currentNote, setCurrentNote] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (isSceneReady && currentNote && sceneRef.current) {
+      requestAnimationFrame(() => {
+        void createNoteBurts(sceneRef.current!, currentNote);
+        createNoteWave(sceneRef.current!, currentNote);
+      });
+    }
+  }, [currentNote, isSceneReady]);
 
   useEffect(() => {
     const run = async () => {
@@ -33,10 +44,13 @@ export default function Animation3D() {
       const size = getSize(container);
 
       // Scene, Camera, Renderer 생성
-      const renderer = new THREE.WebGLRenderer({ antialias: true });
+      const renderer = new THREE.WebGLRenderer({
+        canvas: canvasRef.current!, // 기존 DOM 사용
+        antialias: true,
+      });
       canvasRef.current = renderer.domElement;
       renderer.setSize(size.width, size.height);
-      container.appendChild(renderer.domElement);
+      // container.appendChild(renderer.domElement);
 
       renderer.shadowMap.enabled = true;
       renderer.shadowMap.type = THREE.PCFSoftShadowMap;
@@ -56,18 +70,9 @@ export default function Animation3D() {
       getCanvasText(scene);
       const mixer = await loadCharacter(scene, camera, loadingManager);
       loadLight(scene);
+      const points = loadParticles(scene);
+      particleRef.current = points;
 
-      const particleGroups: THREE.Points[] = [];
-      for (let i = 0; i < 6; i++) {
-        const points = loadParticles(scene);
-        points.position.set(
-          (Math.random() - 0.5) * 23,
-          (Math.random() - 0.5) * 10,
-          (Math.random() - 0.5) * 12
-        );
-        particleGroups.push(points);
-      }
-      particleGroupsRef.current = particleGroups;
       setReady(true);
 
       const clock = new THREE.Clock();
@@ -88,6 +93,7 @@ export default function Animation3D() {
         camera.updateProjectionMatrix();
       }
       window.addEventListener('resize', handleResize);
+      setIsSceneReady(true);
 
       return () => {
         window.removeEventListener('resize', handleResize);
@@ -98,20 +104,14 @@ export default function Animation3D() {
     void run();
   }, []);
 
-  useEffect(() => {
-    if (ready && sceneRef.current && currentNote) {
-      void addNoteText(sceneRef.current, currentNote);
-      console.log(`note: ${currentNote}`);
-    }
-  }, [currentNote, ready]);
-
   return (
     <>
       <div
         ref={mountRef}
-        className="relative z-10 h-5/7 w-4/5 overflow-hidden rounded-2xl"
+        className="relative h-5/7 w-4/5 overflow-hidden rounded-2xl"
       >
         <ProgressBar loadingManager={loadingManager} />
+        <canvas ref={canvasRef} />
       </div>
       <div className="flex items-center justify-center gap-6 pt-2">
         <RecordButton
@@ -126,7 +126,7 @@ export default function Animation3D() {
         <WearableButton bpm={bpm} />
         {ready && (
           <Metronome
-            particleGroups={particleGroupsRef.current}
+            particle={particleRef.current as THREE.Points}
             bpm={bpm}
             onBpmChange={(newBpm) => setBpm(newBpm)}
           />
