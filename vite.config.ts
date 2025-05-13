3a2238204fb8295938ea31187e1b65d8478cf548
CHANGMOK LEE <cmlee5075@gmail.com>
2025-05-14 03:46:45 +0900

    fix: 브라우저에서 SockJS 사용 시 global 참조 오류 해결

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.gltf', '**/*.glb'], // .gltf 파일을 인식
+  define: {
+    global: {},
+  },
 });
